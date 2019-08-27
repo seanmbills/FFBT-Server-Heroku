fbad80b04da1586b07492e0c9544e9a6fdabe425
Sean Bills <seanmichaelbills@gmail.com>
2019-08-26 20:21:02 -0400

    changed port information for heroku implementation

--- src/index.js
+++ src/index.js
@@ -29,6 +29,6 @@ app.get('/', requireAuth, (req, res) => {
     res.send(`Your email: ${req.user.email}`)
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port 3000")
 })
