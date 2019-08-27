e07a003c9b1313a510670dc3b4cb774bab934510
Sean Bills <seanmichaelbills@gmail.com>
2019-08-27 18:23:48 -0400

    created new userRoutes file for updating user info and updating user password (still need to implement)

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const User = mongoose.model('User')
+
+const router = express.Router()
+
+const invalidMessage = "Invalid email or password."
+
+
+
+module.exports = router
