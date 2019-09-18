8603c21d14ee8a97c70feb5c76c3b7be6030e09e
Sean Bills <seanmichaelbills@gmail.com>
2019-09-18 19:22:30 -0400

    created the start of the brewery schema object; still need to add lat/long location sections to the object

---
+++ src/models/Brewery.js
@@ -0,0 +1,473 @@
+const mongoose = require('mongoose')
+
+
+const zipCodeRegex = /^\d{5}$/
+const phoneWithDashRegex = /^\d{3}-\d{3}-\d{4}$/
+const phoneWithoutDashReges = /^\d{10}$/
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+const urlRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+const amPmRegex = /[AaPp][Mm]/
+
+
+const brewerySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        street: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        zipCode: {
+            type: Number,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    return zipCodeRegex.test(v);
+                },
+                message: props => `${props.value} is not a valid phone number!`
+            },
+        }
+    },
+    phoneNumber: {
+        type: String,
+        unique: true,
+        validate: {
+            validator: function(v) {
+                return (phoneWithDashRegex.test(v) || phoneWithoutDashReges.test(v));
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    },
+    email: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return emailRegex.test(v)
+            },
+            message: props => `${props.value} is not a valid email address!`
+        }
+    },
+    website: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return urlRegex.test(v)
+            },
+            message: props => `${props.value} is not a valid URL!`
+        }
+    },
+    businessHours: {
+        Sunday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Monday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Tuesday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Wednesday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Thursday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Friday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Saturday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        }
+    },
+    alternativeKidFriendlyHours: {
+        Sunday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Monday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Tuesday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Wednesday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Thursday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Friday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        },
+        Saturday: {
+            openTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            },
+            closeTime: {
+                time: Number,
+                designator: {
+                    type: String,
+                    validate: {
+                        validator: function(v) {
+                            return amPmRegex.test(v)
+                        },
+                        message: props => `${props.value} is not a valid time!`
+                    }
+                }
+            }
+        }
+    },
+    comments: [
+        // TODO: need to ipmlement this later in sprint 5
+    ],
+    accommodations: {
+        petFriendly: {
+            waterStations: Boolean,
+            indoorSpaces: Boolean,
+            outdoorSpaces: Boolean
+        },
+        friendlyKidAges: {
+            toddlers: Boolean,
+            youngKids: Boolean,
+            teens: Boolean
+        },
+        kidFoodDrinks: {
+            kidFriendlyFood: Boolean,
+            kidFriendlyDrinks: Boolean
+        },
+        changingStations: Boolean,
+        childAccommodations: {
+            games: {
+                indoor: Boolean,
+                outdoor: Boolean
+            },
+            seating: Boolean,
+            strollerSpace: Boolean
+        }
+    }
+})
+
+brewerySchema.pre('save', function(next) {
+    
+})
+
+
+mongoose.model('Brewery', brewerySchema)
