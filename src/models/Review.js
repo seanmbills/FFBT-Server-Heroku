db60d51df0273d93391e063a344ac25d6d43d872
Sean Bills <seanmichaelbills@gmail.com>
2019-09-30 20:58:40 -0400

    edited review schema to update the ratings and comments for the brewery that the review is intended for

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Brewery = mongoose.model('Brewery')
 
 const reviewSchema = new mongoose.Schema({
     message: {
@@ -30,6 +31,35 @@ const reviewSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     }
+})
+
+reviewSchema.pre('save', async function(next) {
+    const review = this
+    
+    const brewery = await Brewery.findById(brewery, async function(err, doc) {
+        if (err) {
+            next(new Error("Must provide a valid brewery id for the review."))
+        }
+
+        try {
+            var newRating = doc.rating * doc.comments.length
+            newRating = newRating * review.rating
+            newRating = newRating / (doc.comments.length + 1)
+
+            var newComments = doc.comments
+            newComments.push(review._id)
+            
+            doc._doc = {...doc._doc, rating: newRating, comments: newComments}
+            doc.markModified('rating')
+            doc.markModified('comments')
+
+            await doc.save()
+        } catch (err) {
+            next(new Error("Experienced an issue when saving this review. Please try again later."))
+        }
+    })
+
+    next()
 })
 
 
